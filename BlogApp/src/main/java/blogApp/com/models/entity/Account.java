7fde2af35cadf326fc04881a25fd4a30604d4600
OoHJoO-478 <jhe00570@gmail.com>
2026-05-18 17:38:33 +0900

    Add database settings and account entity

---
+++ BlogApp/src/main/java/blogApp/com/models/entity/Account.java
@@ -0,0 +1,77 @@
+package blogApp.com.models.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Account {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long accountId;
+	
+	private String accountEmail;
+	
+	private String accountName;
+	
+	private String password;
+	
+	private LocalDateTime registerDate;
+
+	public Account() {
+	}
+
+	public Account(String accountEmail, String accountName, String password, LocalDateTime registerDate) {
+		this.accountEmail = accountEmail;
+		this.accountName = accountName;
+		this.password = password;
+		this.registerDate = registerDate;
+	}
+
+	public Long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getAccountEmail() {
+		return accountEmail;
+	}
+
+	public void setAccountEmail(String accountEmail) {
+		this.accountEmail = accountEmail;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(LocalDateTime registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	
+	
+
+}
